1e3a2bd6075b85c959bcdcef28a28f31ca3be320
NamaniBerryman <91549882+NamaniBerryman@users.noreply.github.com>
2021-10-26 10:25:30 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+
+def exploitOnly():
+    #day1 -> c1,  day2 -> c2, day3 -> c3
+    #297days towards the best cafeteria
+    pass
+
+def exploreOnly():
+    #100 days on each cafeteria
+    pass
+
+def eGreedy(e):
+    #day1 -> c1,  day2 -> c2, day3 -> c3
+    #best cafeteria for most, e value split between the 3 cafeteria's
+    pass
+
+def simulation(t):
+    pass
