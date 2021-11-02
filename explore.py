ca879a41a17c961803ea68ba2b31a71aa434dd90
NamaniBerryman <91549882+NamaniBerryman@users.noreply.github.com>
2021-11-02 10:30:41 -0400

    Update explore.py

--- explore.py
+++ explore.py
@@ -1,14 +1,14 @@
-import random
-
 def exploreOnly():
     C1 = []
     for i in range(100):
         a = random.normalvariate(9,3)
         C1.append(a)
+     #creates a list with all happiness values of C1 visit
     C1sum = 0
     for num in C1:
         C1sum = C1sum + num
-    C1happy = C1sum / 100
+    C1happy = C1sum
+    #adds them all together and defines it as C1happy
 
     C2 = []
     for i in range(100):
@@ -17,7 +17,7 @@ def exploreOnly():
     C2sum = 0
     for num in C2:
         C2sum = C2sum + num
-    C2happy = C2sum / 100
+    C2happy = C2sum
 
     C3 = []
     for i in range(100):
@@ -26,8 +26,12 @@ def exploreOnly():
     C3sum = 0
     for num in C3:
         C3sum = C3sum + num
-    C3happy = C3sum / 100
+    C3happy = C3sum
     #100 days on each cafeteria
+    totalHappy = C1happy + C2happy + C3happy
+    #totalHappy is the sum of all visits of each cafeteria
 
     print("FOR EXPLORE ONLY\nCafeteria 1 Happiness = " + str(C1happy) + "\nCafeteria 2 Happiness = " + str(C2happy) + "\nCafeteria 3 Happiness = " + str(C3happy))
+    print("Total Happiness = " + str(totalHappy))
+    #print statement for reference, shows values when you run the function
     pass
