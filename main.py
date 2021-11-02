eb721a9fcd16e68fb37d9cd76f1ee0f158ed3d85
NamaniBerryman <91549882+NamaniBerryman@users.noreply.github.com>
2021-11-02 10:30:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,12 @@ def exploreOnly():
     for i in range(100):
         a = random.normalvariate(9,3)
         C1.append(a)
+     #creates a list with all happiness values of C1 visit
     C1sum = 0
     for num in C1:
         C1sum = C1sum + num
     C1happy = C1sum
+    #adds them all together and defines it as C1happy
 
     C2 = []
     for i in range(100):
@@ -34,9 +36,11 @@ def exploreOnly():
     C3happy = C3sum
     #100 days on each cafeteria
     totalHappy = C1happy + C2happy + C3happy
+    #totalHappy is the sum of all visits of each cafeteria
 
     print("FOR EXPLORE ONLY\nCafeteria 1 Happiness = " + str(C1happy) + "\nCafeteria 2 Happiness = " + str(C2happy) + "\nCafeteria 3 Happiness = " + str(C3happy))
     print("Total Happiness = " + str(totalHappy))
+    #print statement for reference, shows values when you run the function
     pass
 
 def eGreedy(e):
