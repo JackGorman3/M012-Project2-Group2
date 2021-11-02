656785a119589957e6246c3187cb06a79619e0b4
NamaniBerryman <91549882+NamaniBerryman@users.noreply.github.com>
2021-11-02 10:10:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,35 @@ def exploitOnly():
     pass
 
 def exploreOnly():
+    C1 = []
+    for i in range(100):
+        a = random.normalvariate(9,3)
+        C1.append(a)
+    C1sum = 0
+    for num in C1:
+        C1sum = C1sum + num
+    C1happy = C1sum / 100
+
+    C2 = []
+    for i in range(100):
+        b = random.normalvariate(7,5)
+        C2.append(b)
+    C2sum = 0
+    for num in C2:
+        C2sum = C2sum + num
+    C2happy = C2sum / 100
+
+    C3 = []
+    for i in range(100):
+        c = random.normalvariate(11,7)
+        C3.append(c)
+    C3sum = 0
+    for num in C3:
+        C3sum = C3sum + num
+    C3happy = C3sum / 100
     #100 days on each cafeteria
+
+    print("FOR EXPLORE ONLY\nCafeteria 1 Happiness = " + str(C1happy) + "\nCafeteria 2 Happiness = " + str(C2happy) + "\nCafeteria 3 Happiness = " + str(C3happy))
     pass
 
 def eGreedy(e):
